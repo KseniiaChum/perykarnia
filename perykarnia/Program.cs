5db01b4570f31f755be9b011ac13352688960ff7
school <school@A45PC15>
2025-03-11 09:42:16 +0200

    gfgf

--- perykarnia/Program.cs
+++ perykarnia/Program.cs
@@ -12,7 +12,8 @@
 	Додавання(вилучення. редагування)   майстра(перукаря, косметолога у(з)  базу
 	Додавання(вилучення. редагування)  продуктів у(з)  базу
 	Можливість  попереднього замовлення послуги 
-	Реєстрація виконання послуги(клієнт, послуга, дата, виконавець)
+	Реєстрація виконання послуги(клієнт, послуга, дата, виконавець) що за послуга
+клас клієнт клас майстра стаж тощо,клас попереднього засовдення* також тип послуг
 	*/
 
 
@@ -26,3 +27,24 @@ Console.WriteLine("\t4. Show all services");
 Console.WriteLine("\t5. Dalete services");
 Console.WriteLine("\t6. Sell services");
 Console.WriteLine("\t7. register services");
+
+public class Perycarnia
+{
+	public string Name { get; set;}
+    public double Prise { get; set; }
+    public double Category{ get; set; }
+}
+
+public class Нairdresser
+{
+    public string Name { get; set; }
+    public string Experience { get; set; }
+    public string Age { get; set; }
+}
+
+public class Client
+{
+    public string Name { get; set; }
+    public string haircut { get; set; }
+    public string hour { get; set; }
+}
